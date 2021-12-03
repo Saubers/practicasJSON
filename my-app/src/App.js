49a51addb9e9cdfc7cfc2decef57f3804d396f5c
Ramos Felipe <Felipeofor@gmail.com>
2021-12-03 14:39:45 -0300

    lista de categorias

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,21 +5,30 @@ import React from 'react';
 
 function App() {
 
+var _ = require('lodash');
 const productos = require('./products.json');
+const categoriaProductos = productos.results
 
-console.log(productos);
+//categorias
+//categoriaProductos.forEach(item => console.log(item.categoriaProducto.name));
 
-    
+//mapeo de categorias
+var lista = _.map(categoriaProductos, categoriaProductos.name);
+
+//impresion de categorias
+const listaImpresa = lista.map((item) => <li key={item.id}>{item.name}</li>);
+
+
   return (
     <React.Fragment>
       <h1>Ejercicio JSON, abrir consola</h1>
+      Ingrese una categoria
+    <input type="text" name="categoriaProduct"/>
+    <ul>
+      { listaImpresa }
+    </ul>
     </React.Fragment>
   );
 }
 
 export default App;
-
-
-
-
-
