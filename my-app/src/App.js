d4b56e94ef0003ddbfba95f7e995c186a7d0f340
Ramos Felipe <Felipeofor@gmail.com>
2021-12-06 12:20:05 -0300

    filtro de productos por precio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,7 +13,7 @@ const categoriaProductos = productos.results;
 //Suma de precios de los productos
 let cuantiti = 0
 categoriaProductos.forEach(item => { cuantiti = cuantiti + item.price });
-//console.log(cuantiti);
+console.log(cuantiti);
 
 /* const nombre = {};
 nombre.pepe = "pepe";
@@ -29,6 +29,7 @@ console.log(nombre); */
 
 //mapeo de categorias
 var lista = _.map(categoriaProductos, categoriaProductos.name);
+
 
 
 //Guardar categorias en un array
@@ -53,21 +54,31 @@ console.log(objeto);
 
 //Itero sobre el objeto
 
-for(const propiedad in objeto) {
+/* for(const propiedad in objeto) {
   console.log(`${propiedad}: ${objeto[propiedad]}`);
-}
+} */
+
+for(const propiedad in objeto) {
+  if(categoriasOrdenadas.map((item) => item) === objeto.hasOwnProperty[propiedad]) {Object.defineProperty(objeto, "{propiedad}", {value: categoriasOrdenadas[propiedad]});}}
+console.log(objeto);
+
 
 //Cuantos porductos de cocina y cuantos de bar??? grab_for
 //Happy hour => total
 //status, cuantos estas activos o no?
+
 //cuantos productos valen mas o menos de $500
+const productosValenMas = lista.filter(item => item.price > 500);
+const productosValenMenos = lista.filter(item => item.price < 500);
+console.log(`cantidad de productos que valen mas de $500: ${productosValenMas.length}`);
+console.log(`cantidad de productos que valen menos de $500: ${productosValenMenos.length}`);
 //Cuantos productos tienen al menos un grupo de opciones.
 
 
   return (
     <React.Fragment>
       <h1>Ejercicio JSON, abrir consola</h1>
-      Ingrese una categoria
+      Ingrese una categoria   
     <input type="text" name="categoriaProduct"/>
     <h3>Categorias disponibles:</h3>
     <ol>
